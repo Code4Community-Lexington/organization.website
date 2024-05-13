3cd0de966f397b91265b38c97e803920be0ddf4b
nyefan <git@nyefan.org>
2024-05-12 20:51:19 -0400

    feat: add configurable base-url to accommodate github-pages previews and non-custom-url deployments

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://www.code4community.dev",
   output: "static",
+  site: "https://www.code4community.dev",
+  trailingSlash: "never",
 });
