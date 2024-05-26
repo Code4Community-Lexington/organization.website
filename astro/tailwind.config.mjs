050bde558da068bed0a8602f9225e3ce73a8d1b5
Neal <neal.grindstaff@gmail.com>
2024-05-26 08:15:17 -0400

    add colors with content-agnostic names

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"bg-primary": "#272d2a",
+				"bg-secondary-1": "#8fbb98",
+				"bg-secondary-2": "#37928d",
+				"bg-secondary-3": "#e3c699"
+			}
+		},
 	},
 	plugins: [],
 }
