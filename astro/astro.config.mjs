8cad42171d1cbb54bdd0273e1207e3b939d3f40f
Neal <neal.grindstaff@gmail.com>
2024-05-26 08:03:00 -0400

    added tailwind package

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://www.code4community.dev",
   trailingSlash: "never",
+  integrations: [tailwind()]
 });
